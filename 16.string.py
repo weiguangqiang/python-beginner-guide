2d77d323d94067f58b9a25c199e3720fc51d6723
weiguangqiang <weiguangqiang820@gmail.com>
2016-07-07 23:13:45 +0800

    Add comment and string

---
+++ 16.string.py
@@ -0,0 +1,20 @@
+# !/usr/bin/Python
+
+# website: http://pythonprogramminglanguage.com/strings-and-sub-strings
+
+str = "This is string"
+
+print(str)
+
+# get some one character of string
+# Eg. get the third character of str
+print(str[2])
+
+# we can slice[切断] the string
+# Eg
+print(str[:6])
+print(str[2:])
+print(str[3:19])
+
+sliceStr = str[0:4]
+print('sliceStr: ', sliceStr)
